6c880d63d873e410aef08d95ff29d18ea72d6b42
Vycheslav Tykhonov <4tikhonov@gmail.com>
2016-06-07 16:12:53 +0100

    Aggregator test updated

--- unit-tests/aggregator-test.py
+++ unit-tests/aggregator-test.py
@@ -7,9 +7,10 @@ import re
 from pymongo import MongoClient
 from werkzeug import secure_filename
 from nltk import tokenize
-from xmlprocessor import *
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../clioinfra.js/modules')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from storage import data2store, readdata
+from core.xmlprocessor import *
 
 pid = "ddd:010285299:mpeg21:a0213"
 dbname = "kbstorage"
